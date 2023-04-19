0eaff0dd53edb454c805343e17d596aede7425ee
KhSanzhar <sanzhkhalel@gmail.com>
2023-04-19 17:43:30 +0600

    lab10

---
+++ lab10/lab10front/src/app/models/models.ts
@@ -0,0 +1,21 @@
+export interface Company {
+  id: number,
+  name: string,
+  description: string,
+  city: string,
+  address: string
+}
+
+export interface Vacancy {
+  id: number;
+  name: string;
+  description: string;
+  salary: number;
+  company: {
+    id: number;
+    name: string;
+    description: string;
+    city: string;
+    address: string;
+  };
+}
